eb7c2d1d0b83465eef9c51d25ae03439bc044fe6
Phillip Gore <phillipagore@me.com>
2014-02-13 16:40:43 -0600

    csv download

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,41 +1,41 @@
 $( document ).ready(function() {
 
 	$('body').on('click', 'a', function(e) {
-		e.preventDefault();
-	
-		if (!$(this).hasClass('outside_link')) {
+		if (!$(this).hasClass('download')) {
 			e.preventDefault();
-			$('.cover').fadeIn('fast');
-			$('.container').load($(this).prop('href') + " .content", function() {
-				$('.cover').fadeOut('fast');
-			});
-			History.pushState(null, "Discover Christ's Church", $(this).attr('href'));
+			if (!$(this).hasClass('outside_link')) {
+				e.preventDefault();
+				$('.cover').fadeIn('fast');
+				$('.container').load($(this).prop('href') + " .content", function() {
+					$('.cover').fadeOut('fast');
+				});
+				History.pushState(null, "Discover Christ's Church", $(this).attr('href'));
+			}
 		}
 	});
 	
 	$('body').on('submit', 'form', function(e) {
-		e.preventDefault();
-		
-		$('.cover').fadeIn('fast');
-		$.ajax({
-			url: $(this).prop('action'),
-			type: $(this).prop('method'),
-			datatype: 'html',
-			data: $(this).serialize(),
-			success: function(data){
-				if (data.charAt(0) === "1") {
-					$('.cover').fadeOut('fast');
-					$('.jsNotice').html(data.slice(2)).slideDown();
-				} else {
-					$('.container').load(data + " .content", function() {
+			e.preventDefault();
+			$('.cover').fadeIn('fast');
+			$.ajax({
+				url: $(this).prop('action'),
+				type: $(this).prop('method'),
+				datatype: 'html',
+				data: $(this).serialize(),
+				success: function(data){
+					if (data.charAt(0) === "1") {
 						$('.cover').fadeOut('fast');
-					});
-					History.pushState(null, "Discover Christ's Church", data);
-					console.log(data)
+						$('.jsNotice').html(data.slice(2)).slideDown();
+					} else {
+						$('.container').load(data + " .content", function() {
+							$('.cover').fadeOut('fast');
+						});
+						History.pushState(null, "Discover Christ's Church", data);
+						console.log(data)
+					}
 				}
-			}
-		});
-		return false;
+			});
+			return false;
 	});
 	
 	
